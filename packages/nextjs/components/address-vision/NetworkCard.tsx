b0419de73b0dbdeeeb826e3b25644baad6f7c309
port <108868128+portdeveloper@users.noreply.github.com>
2023-10-07 23:32:00 +0300

    Add NetworkCard, modify index.tsx

---
+++ packages/nextjs/components/address-vision/NetworkCard.tsx
@@ -0,0 +1,208 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Address as AddressComp } from "../scaffold-eth";
+import { CovalentClient } from "@covalenthq/client-sdk";
+import { useDarkMode } from "usehooks-ts";
+import { Address, formatEther, isAddress } from "viem";
+import { Chain } from "wagmi";
+
+const isValidEnsOrAddress = (name: string) => isAddress(name) || /^[a-z0-9-]+\.eth$/.test(name); // @remind move this to utils
+
+const client = new CovalentClient(process.env.NEXT_PUBLIC_COVALENT_API_KEY as string);
+
+const getChainNameForOpensea = (id: number) => {
+  switch (id) {
+    case 1:
+      return "ethereum";
+    case 42161:
+      return "arbitrum";
+    case 10:
+      return "optimism";
+    case 8453:
+      return "base";
+    case 137:
+      return "matic";
+    default:
+      return "ethereum";
+  }
+};
+
+const getChainNameForCovalent = (id: number) => {
+  switch (id) {
+    case 1:
+      return "eth-mainnet";
+    case 42161:
+      return "arbitrum-mainnet";
+    case 10:
+      return "optimism-mainnet";
+    case 8453:
+      return "base-mainnet";
+    case 137:
+      return "matic-mainnet";
+    default:
+      return "eth-mainnet";
+  }
+};
+
+export const NetworkCard = ({ someAddress, chain }: { someAddress: Address; chain: Chain }) => {
+  const [nfts, setNfts] = useState<any[]>([]);
+  const [tokenBalances, setTokenBalances] = useState<any[]>([]);
+  const { isDarkMode } = useDarkMode();
+
+  const getNfts = async () => {
+    const options = {
+      method: "GET",
+      headers: { accept: "application/json", "x-api-key": process.env.NEXT_PUBLIC_OPENSEA_API_KEY || "default-key" },
+    };
+
+    try {
+      const response = await fetch(
+        `https://api.opensea.io/api/v2/chain/${getChainNameForOpensea(chain.id)}/account/${someAddress}/nfts`,
+        options,
+      );
+      const data = await response.json();
+
+      if (data.nfts && data.nfts.length > 0) {
+        const nftData = [];
+        for (let i = 0; i < Math.min(10, data.nfts.length); i++) {
+          const nft = data.nfts[i];
+          console.log(nft);
+          if (nft.image_url && nft.identifier !== "0") {
+            nftData.push({
+              imageUrl: nft.image_url,
+              contract: nft.contract,
+              identifier: nft.identifier,
+            });
+          }
+        }
+        setNfts(nftData);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getTokens = async () => {
+    const res = await client.BalanceService.getTokenBalancesForWalletAddress(
+      getChainNameForCovalent(chain.id),
+      someAddress,
+      {
+        nft: false,
+        noSpam: true,
+      },
+    );
+    if (res.data && res.data.items) {
+      const filteredTokens = res.data.items ? res.data.items.filter(token => token.quote !== 0) : [];
+      setTokenBalances(filteredTokens);
+    }
+  };
+
+  useEffect(() => {
+    setNfts([]);
+    setTokenBalances([]);
+    if (someAddress && isValidEnsOrAddress(someAddress)) {
+      getNfts();
+      getTokens();
+    }
+  }, [someAddress]);
+
+  if (someAddress && isValidEnsOrAddress(someAddress)) {
+    return (
+      <div className="card w-[425px] bg-base-100 shadow-xl flex-grow">
+        <div className="card-body">
+          <h2 className="card-title whitespace-nowrap">
+            <AddressComp address={someAddress} /> on {chain.name}
+          </h2>
+          <h3 className="font-bold">NFTs</h3>
+          {nfts.length > 0 ? (
+            <div className="flex flex-col relative">
+              <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-secondary rounded-box">
+                {nfts.map((nft, index) => (
+                  <div className="carousel-item" key={index}>
+                    <a
+                      href={`https://opensea.io/assets/${getChainNameForOpensea(chain.id)}/${nft.contract}/${
+                        nft.identifier
+                      }`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="h-32 w-32 flex justify-center items-center"
+                    >
+                      <div className="h-full w-full flex justify-center items-center">
+                        <Image
+                          src={nft.imageUrl}
+                          className="rounded-box object-contain h-full w-full"
+                          alt={`NFT ${index}`}
+                          width={128}
+                          height={128}
+                        />
+                      </div>
+                    </a>
+                  </div>
+                ))}
+              </div>
+
+              <div className="self-end flex gap-2 absolute bottom-[-35px] right-3">
+                <p className="text-xs">See more on </p>
+                <Link
+                  href={`https://opensea.io/${someAddress}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="flex"
+                >
+                  <Image
+                    src={isDarkMode ? "/opensea-logo-light.svg" : "/opensea-logo-dark.svg"}
+                    alt="opensea logo"
+                    width={70}
+                    height={20}
+                  />
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <p>No NFTs found.</p>
+          )}
+          <h3 className="mt-4 font-bold">Tokens</h3>
+          {tokenBalances.length > 0 ? (
+            <div className="overflow-x-auto max-h-48">
+              <table className="table table-zebra">
+                <thead>
+                  <tr>
+                    <th>Token</th>
+                    <th>Balance</th>
+                    <th>Balance in USD</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tokenBalances.slice(0, 10).map((token, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{`${token.contract_name} (${token.contract_ticker_symbol})`}</td>
+                        <td>{Number(formatEther(token.balance)).toFixed(2)}</td>
+                        <td>≈${token.quote?.toFixed(2)}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <p>No tokens found.</p>
+          )}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card w-[425px] bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">See someone on ??????????</h2>
+          <p>some text</p>
+          <div className="card-actions justify-end flex items-center text-xl p-4">
+            Balance: <p>search for an address</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import Head from "next/head";
 import type { NextPage } from "next";
 import { QRCodeSVG } from "qrcode.react";
+import { isAddress } from "viem";
+import * as chains from "wagmi/chains";
+import { NetworkCard } from "~~/components/address-vision/NetworkCard";
 import { Address, AddressInput, Balance } from "~~/components/scaffold-eth";
+
+// @todo add chain logos to cards for better UX
+const isValidEnsOrAddress = (name: string) => isAddress(name) || /^[a-z0-9-]+\.eth$/.test(name); // @remind move this to utils
 
 const Home: NextPage = () => {
   const [someAddress, setSomeAddress] = useState("");
@@ -10,7 +16,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Address.vision</title>
+        <title>address.vision</title>
         <meta name="description" content="address vision" />
       </Head>
       <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 grid grid-cols-12">
@@ -31,140 +37,129 @@ const Home: NextPage = () => {
       </div>
 
       {someAddress ? (
-        <div className="flex w-full items-center flex-col flex-grow p-10 gap-4">
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div className="card w-96 bg-base-100 shadow-xl flex flex-col">
-              <div className="card-body flex-grow">
-                <h2 className="card-title">
-                  {someAddress && (
-                    <div>
-                      <Address address={someAddress} size="3xl" />
-                    </div>
+        <div className="flex w-full items-center flex-col flex-grow p-4 mt-4 gap-4">
+          <div className="flex flex-wrap">
+            <div className="w-full sm:w-1/2 lg:w-1/3 p-4 space-y-4">
+              <div className="card w-[425px] bg-base-100 shadow-xl flex flex-col">
+                <div className="card-body flex-grow pb-0">
+                  <h2 className="card-title">
+                    {someAddress && (
+                      <div>
+                        <Address address={someAddress} size="4xl" />
+                      </div>
+                    )}
+                  </h2>
+                </div>
+                <div className="card-actions justify-end flex items-center text-xl p-4">
+                  Balance:{" "}
+                  {someAddress && isValidEnsOrAddress(someAddress) ? (
+                    <Balance address={someAddress} targetNetwork={chains.mainnet} className="text-2xl" />
+                  ) : (
+                    <p>search for an address</p>
                   )}
-                </h2>
+                </div>
               </div>
-              <div className="card-actions justify-end flex items-center text-xl p-4">
-                Balance:{" "}
-                {someAddress ? <Balance address={someAddress} className="text-2xl" /> : <p>search for an address</p>}
+
+              <div className="card w-[425px] bg-base-100 shadow-xl">
+                <div className="card-body">
+                  <h2 className="card-title">
+                    See
+                    {someAddress ? (
+                      <div>
+                        <Address address={someAddress} />
+                      </div>
+                    ) : (
+                      <p>someone</p>
+                    )}
+                    on
+                  </h2>
+                  <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
+                    <button
+                      className="btn btn-xs btn-primary rounded-full"
+                      title="View on Etherscan"
+                      tabIndex={1}
+                      onClick={() => {
+                        window.open("https://etherscan.io/address/" + someAddress, "_blank");
+                      }}
+                    >
+                      Etherscan
+                    </button>
+                    <button
+                      className="btn btn-xs btn-primary rounded-full"
+                      title="View on Blockscan"
+                      tabIndex={2}
+                      onClick={() => {
+                        window.open("https://blockscan.com/address/" + someAddress, "_blank");
+                      }}
+                    >
+                      Blockscan
+                    </button>
+                    <button
+                      className="btn btn-xs btn-primary rounded-full"
+                      title="View on Optimistic Etherscan"
+                      tabIndex={3}
+                      onClick={() => {
+                        window.open("https://optimistic.etherscan.io/address/" + someAddress, "_blank");
+                      }}
+                    >
+                      Op Etherscan
+                    </button>
+                    <button
+                      className="btn btn-xs btn-primary rounded-full"
+                      title="View on Zerion"
+                      tabIndex={4}
+                      onClick={() => {
+                        window.open("https://app.zerion.io/" + someAddress + "/overview", "_blank");
+                      }}
+                    >
+                      Zerion
+                    </button>
+                    <button
+                      className="btn btn-xs btn-primary rounded-full"
+                      title="View on Zapper"
+                      tabIndex={5}
+                      onClick={() => {
+                        window.open("https://zapper.xyz/account/" + someAddress, "_blank");
+                      }}
+                    >
+                      Zapper
+                    </button>
+                    <button
+                      className="btn btn-xs btn-primary rounded-full"
+                      title="View on Safe"
+                      tabIndex={6}
+                      onClick={() => {
+                        window.open("https://app.safe.global/transactions/queue?safe=eth:" + someAddress, "_blank");
+                      }}
+                    >
+                      Safe
+                    </button>
+                  </div>
+                </div>
               </div>
+              <NetworkCard someAddress={someAddress} chain={chains.arbitrum} />
+              <NetworkCard someAddress={someAddress} chain={chains.base} />
             </div>
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">
-                  See
+            <div className="w-full sm:w-1/2 lg:w-1/3 p-4 space-y-4">
+              <div className="card w-[425px] bg-base-100 shadow-xl">
+                <div className="card-body flex justify-center items-center ">
                   {someAddress ? (
-                    <div>
-                      <Address address={someAddress} />
+                    <div className="flex flex-col">
+                      <QRCodeSVG value={someAddress} size={350} />
                     </div>
                   ) : (
-                    <p>someone</p>
+                    <div>QR code will appear here</div>
                   )}
-                  on
-                </h2>
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
-                  <button
-                    className="btn btn-primary p-2 m-1 rounded-full"
-                    title="View on Etherscan"
-                    tabIndex={1}
-                    onClick={() => {
-                      window.open("https://etherscan.io/address/" + someAddress, "_blank");
-                    }}
-                  >
-                    Etherscan
-                  </button>
-                  <button
-                    className="btn btn-primary p-2 m-1 rounded-full"
-                    title="View on Blockscan"
-                    tabIndex={2}
-                    onClick={() => {
-                      window.open("https://blockscan.com/address/" + someAddress, "_blank");
-                    }}
-                  >
-                    Blockscan
-                  </button>
-                  <button
-                    className="btn btn-primary p-2 m-1 rounded-full"
-                    title="View on Optimistic Etherscan"
-                    tabIndex={3}
-                    onClick={() => {
-                      window.open("https://optimistic.etherscan.io/address/" + someAddress, "_blank");
-                    }}
-                  >
-                    Op Etherscan
-                  </button>
-                  <button
-                    className="btn btn-primary p-2 m-1 rounded-full"
-                    title="View on Zerion"
-                    tabIndex={4}
-                    onClick={() => {
-                      window.open("https://app.zerion.io/" + someAddress + "/overview", "_blank");
-                    }}
-                  >
-                    Zerion
-                  </button>
-                  <button
-                    className="btn btn-primary p-2 m-1 rounded-full"
-                    title="View on Zapper"
-                    tabIndex={5}
-                    onClick={() => {
-                      window.open("https://zapper.xyz/account/" + someAddress, "_blank");
-                    }}
-                  >
-                    Zapper
-                  </button>
-                  <button
-                    className="btn btn-primary p-2 m-1 rounded-full"
-                    title="View on Safe"
-                    tabIndex={6}
-                    onClick={() => {
-                      window.open("https://app.safe.global/transactions/queue?safe=eth:" + someAddress, "_blank");
-                    }}
-                  >
-                    Safe
-                  </button>
                 </div>
               </div>
+              <NetworkCard someAddress={someAddress} chain={chains.optimism} />
             </div>
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Card title!</h2>
-                <p>some text</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">action!</button>
-                </div>
-              </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Card title!</h2>
-                <p>some text</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">action!</button>
-                </div>
-              </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Card title!</h2>
-                <p>some text</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">action!</button>
-                </div>
-              </div>
-            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 p-4 space-y-4">
+              <NetworkCard someAddress={someAddress} chain={chains.mainnet} />
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body flex justify-center items-center">
-                {someAddress ? (
-                  <div className="flex flex-col">
-                    <QRCodeSVG value={someAddress} size={256} />
-                  </div>
-                ) : (
-                  <div className="text-gray-400">QR code will appear here</div>
-                )}
-              </div>
+              <NetworkCard someAddress={someAddress} chain={chains.polygon} />
             </div>
           </div>
         </div>
