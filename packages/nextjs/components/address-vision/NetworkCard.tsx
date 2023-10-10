0af9f9857d96e6e5df69b4b68e06eeb173048248
port <108868128+portdeveloper@users.noreply.github.com>
2023-10-10 12:21:32 +0300

    Implement a skeleton ui in NetworkCard

--- packages/nextjs/components/address-vision/NetworkCard.tsx
+++ packages/nextjs/components/address-vision/NetworkCard.tsx
@@ -206,12 +206,51 @@ export const NetworkCard = ({ someAddress, chain }: { someAddress: Address; chai
     );
   } else {
     return (
-      <div className="card w-[425px] bg-base-100 shadow-xl">
+      <div className="card w-[370px] md:w-[425px] bg-base-100 shadow-xl flex-grow animate-pulse">
         <div className="card-body">
-          <h2 className="card-title">See someone on ??????????</h2>
-          <p>some text</p>
-          <div className="card-actions flex items-center justify-end p-4 text-xl">
-            Balance: <p>search for an address</p>
+          <div className="flex items-center space-x-4">
+            <div className="rounded-md bg-slate-300 h-6 w-6"></div>
+            <div className="h-2 w-28 bg-slate-300 rounded"></div>
+          </div>
+
+          <h3 className="font-bold mt-4">NFTs</h3>
+          <div className="relative flex flex-col">
+            <div className="carousel-center carousel rounded-box max-w-md space-x-4 bg-secondary p-4">
+              <div className="carousel-item">
+                <div className="rounded-md bg-slate-300 h-32 w-32"></div>
+              </div>
+              <div className="carousel-item">
+                <div className="rounded-md bg-slate-300 h-32 w-32"></div>
+              </div>
+              <div className="carousel-item">
+                <div className="rounded-md bg-slate-300 h-32 w-32"></div>
+              </div>
+            </div>
+          </div>
+
+          <h3 className="font-bold mt-4">Tokens</h3>
+          <div className="max-h-48 overflow-x-auto">
+            <table className="table table-zebra">
+              <thead>
+                <tr>
+                  <th>Token</th>
+                  <th>Balance</th>
+                  <th>Balance in USD</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="h-2 w-28 bg-slate-300"></td>
+                  <td className="h-2 w-16 bg-slate-300"></td>
+                  <td className="h-2 w-20 bg-slate-300"></td>
+                </tr>
+                <tr>
+                  <td className="h-2 w-28 bg-slate-300"></td>
+                  <td className="h-2 w-16 bg-slate-300"></td>
+                  <td className="h-2 w-20 bg-slate-300"></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
