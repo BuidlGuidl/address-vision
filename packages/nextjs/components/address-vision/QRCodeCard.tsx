a9b987c6ef9d230ead112a9b3729a6ef41b6027f
port <108868128+portdeveloper@users.noreply.github.com>
2023-10-10 12:30:37 +0300

    Add skeleton ui to QRCodeCard

--- packages/nextjs/components/address-vision/QRCodeCard.tsx
+++ packages/nextjs/components/address-vision/QRCodeCard.tsx
@@ -1,7 +1,21 @@
 import { QRCodeSVG } from "qrcode.react";
-import { Address } from "viem";
+import { Address, isAddress } from "viem";
 
 export const QRCodeCard = ({ someAddress }: { someAddress: Address }) => {
+  if (!isAddress(someAddress))
+    return (
+      <div className="card w-[370px] md:w-[425px] bg-base-100 shadow-xl">
+        <div className="card-body flex items-center justify-center">
+          <div className="hidden md:block animate-pulse">
+            <div className="h-[350px] w-[350px] bg-slate-300 rounded"></div>
+          </div>
+          <div className="block md:hidden animate-pulse">
+            <div className="h-[250px] w-[250px] bg-slate-300 rounded"></div>
+          </div>
+        </div>
+      </div>
+    );
+
   return (
     <div className="card w-[370px] md:w-[425px] bg-base-100 shadow-xl">
       <div className="card-body flex items-center justify-center">
